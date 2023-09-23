dcbb386bfb0a25bac93b907e6135fa76347b84d6
P4R4NOIC <rvindas-hernandez@hotmail.com>
2023-09-22 23:03:01 -0600

    Update HTML
    Co-Authored-By: fernandogross06 <80482002+fernandogross06@users.noreply.github.com>

---
+++ Archivos JS/evalFunc.js
@@ -0,0 +1,21 @@
+var contenedorPreguntas = document.getElementById("preguntas");
+var presionado = new Boolean(false)
+
+function generarPreguntas(value){
+
+    if(presionado == false){
+    
+        var i = value-1;
+        document.getElementById('lab').innerHTML = 'Pregunta ' + (i+1)
+    
+        for(i; i> 0; i--){
+            var elem = document.querySelector(".preguntas");
+            var clone = elem.cloneNode(true);
+            clone.id = "preguntas";
+            document.getElementById('lab').innerHTML = 'Pregunta ' + i
+            elem.after(clone);  
+        }
+    }
+    presionado = true;
+    
+}
